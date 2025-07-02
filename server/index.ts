3ae7b2e661e8d13e498eaa3b8e98886a648064d0
johnson <oyebodej2@gmail.com>
2025-07-02 21:15:53 +0100

    fix: resolving enviroment variables

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import cors from 'cors';
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 import path from 'path';
 import { DatabaseManager } from './database/index.js';
 import { authRoutes } from './routes/auth.js';
